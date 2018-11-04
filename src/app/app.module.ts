5acbf4acb780fcdf21f6c4d318f9806bd1bbca04
Edmund Wright <nekurasky@gmail.com>
2018-11-04 17:31:57 -0600

    Generated all of the components and modules that will be needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
+import { WikiComponent } from './wiki/wiki.component';
+import { GiphyComponent } from './giphy/giphy.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WikiSearchService } from './wiki/wiki-search.service';
+import { GiphySearchService } from './giphy/giphy-search.service';
+import { HistoryService } from './history/history.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    WikiComponent,
+    GiphyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [WikiSearchService, GiphySearchService, HistoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
